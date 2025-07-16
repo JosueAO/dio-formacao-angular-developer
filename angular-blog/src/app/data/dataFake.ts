c73287277fec368bdb002fa3d56f5a8034017935
Josue A. Oliveira <josueao.mobile@gmail.com>
2025-07-16 19:27:07 -0300

    feat: implementa sistema de dados fake e @Input properties
     Funcionalidades implementadas:
    - Sistema de dados fake com artigos Marvel (dataFake.ts)
    - Interface TypeScript para tipagem dos artigos (Article)
    - @Input properties nos componentes BigCard e SmallCard
    - Binding dinâmico na página Home com dados reais
    - Layout responsivo melhorado seguindo projeto original
    
     Melhorias Angular 20:
    - Standalone components mantidos
    - Property binding moderno com []
    - Interface para type safety
    - Estrutura de dados organizada
    
     Componentes atualizados:
    - BigCard: photoCover, cardTitle, cardDescription, Id
    - SmallCard: photoCover, cardTitle, Id
    - Home: integração com dados dinâmicos
    - CSS responsivo aprimorado

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+import { Article } from './article.interface';
+
+export const dataFake: Article[] = [
+  {
+    id: "1",
+    title: "NOVO HOMEM DE FERRO EM 3D",
+    description: "Marvel anuncia um novo filme do homem de ferro, confira",
+    photoCover: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    id: "2",
+    title: "Nova Série anunciada no Disney +",
+    description: "Disney+ anuncia nova série do universo Marvel com personagens inéditos",
+    photoCover: "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    id: "3",
+    title: "Novo filme do Pantera Negra lançado em breve",
+    description: "Wakanda Forever continua o legado do herói em uma nova aventura épica",
+    photoCover: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    id: "4",
+    title: "Novo filme dos X-men está em pós produção, afirma diretor",
+    description: "O diretor confirma que o próximo filme dos mutantes está em desenvolvimento avançado",
+    photoCover: "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+];
--- angular-blog/src/app/pages/home/home.ts
+++ angular-blog/src/app/pages/home/home.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { MenuTitle } from "../../components/menu-title/menu-title";
 import { BigCard } from "../../components/big-card/big-card";
+import { SmallCard } from "../../components/small-card/small-card";
+import { dataFake } from '../../data/dataFake';
+import { Article } from '../../data/article.interface';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitle, BigCard],
+  imports: [MenuTitle, BigCard, SmallCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  articles: Article[] = dataFake;
 }
